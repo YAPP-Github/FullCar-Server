c77b9387e3d143b4977cd8e808bea3a9c61c2915
seokyeong <purewater71@naver.com>
2023-12-11 19:00:03 +0900

    config: initial setting

---
+++ src/main/java/com/fullcar/FullcarApplication.java
@@ -0,0 +1,13 @@
+package com.fullcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullcarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fullcar/FullcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
