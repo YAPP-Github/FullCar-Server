085bdaad5e4fad18459c21e9d4876c5c04b4dabd
Seokyeong <purewater71@naver.com>
2024-01-20 02:01:26 +0900

    fix: deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
+source ~/.bashrc
 $(aws ecr get-login --region ap-northeast-2 --no-include-email)
 sudo docker rm -f codedeploy
-sudo docker rmi ECR_REPOSITORY_NAME
-sudo docker pull ECR_REPOSITORY_NAME
-sudo docker run -d --name codedeploy -p 8080:80 ECR_REPOSITORY_NAME
+sudo docker rmi "$ECR_REPOSITORY"
+sudo docker pull "$ECR_REPOSITORY"
+sudo docker run -d --name codedeploy -p 8080:80 "$ECR_REPOSITORY"
