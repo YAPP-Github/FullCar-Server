0f5025dc156cf980169b14be6394c545e54ea767
Seokyeong <purewater71@naver.com>
2024-01-20 03:37:08 +0900

    chore: touched deploy.sh in deploy.yml

--- scripts/deploy.sh
+++
@@ -1,6 +0,0 @@
-source ~/.bashrc
-$(aws ecr get-login --region ap-northeast-2 --no-include-email)
-sudo docker rm -f codedeploy
-sudo docker rmi "$ECR_REPOSITORY"
-sudo docker pull "$ECR_REPOSITORY"
-sudo docker run -d --name codedeploy -p 8080:80 --restart always "$ECR_REPOSITORY"
