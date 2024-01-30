e9a890ad67922cc09bbd2aed9c8474f797ef23d5
Young Hun Park <pyhpyh0670@gmail.com>
2024-01-31 00:01:03 +0900

    fix: Resolve conflict

--- src/main/java/com/fullcar/carpool/application/CarpoolMapper.java
+++ src/main/java/com/fullcar/carpool/application/CarpoolMapper.java
@@ -26,28 +26,9 @@ public class CarpoolMapper {
                 .money(carpool.getCost().getMoney())
                 .content(carpool.getContent())
                 .moodType(carpool.getMoodType())
-                .gender(member.getGender())
-                .createdAt(carpool.getCreatedAt())
-                .build();
-    }
-
-    public CarpoolResponseDto.CarpoolDetailDTO toDetailDto(Carpool carpool, Member member, Car car) {
-        return CarpoolResponseDto.CarpoolDetailDTO.builder()
-                .id(carpool.getCarpoolId().getId())
-                .pickupLocation(carpool.getPickupLocation())
-                .periodType(carpool.getCost().getPeriodType())
-                .money(carpool.getCost().getMoney())
-                .content(carpool.getContent())
-                .moodType(carpool.getMoodType())
-                .carpoolState(carpool.getCarpoolState())
                 .companyName(member.getCompany())
                 .gender(member.getGender())
                 .createdAt(carpool.getCreatedAt())
-                .nickname(member.getNickname())
-                .carNo(car.getCarNo())
-                .carName(car.getCarName())
-                .carBrand(car.getCarBrand())
-                .carColor(car.getCarColor())
                 .build();
     }
 
