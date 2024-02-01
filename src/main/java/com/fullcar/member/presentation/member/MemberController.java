d5a4eeeb663422121b48f043a53ac8db2aa820f6
Seokyeong <purewater71@naver.com>
2024-02-02 00:50:56 +0900

    feat: get member's information API

--- src/main/java/com/fullcar/member/presentation/member/MemberController.java
+++ src/main/java/com/fullcar/member/presentation/member/MemberController.java
@@ -6,16 +6,14 @@ import com.fullcar.core.response.SuccessCode;
 import com.fullcar.member.application.member.MemberService;
 import com.fullcar.member.domain.member.Member;
 import com.fullcar.member.presentation.member.dto.request.CompanyRequestDto;
+import com.fullcar.member.presentation.member.dto.response.MemberGetResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,5 +32,16 @@ public class MemberController {
     public ApiResponse<Object> postCompany(@CurrentMember Member member, @RequestBody @Valid CompanyRequestDto companyRequestDto) {
         memberService.registerCompany(member, companyRequestDto);
         return ApiResponse.success(SuccessCode.REGISTER_SUCCESS);
+    }
+
+    @Operation(summary = "회원 정보 조회 API")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "조회 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "서버 내부 오류", content = @Content)
+    })
+    @GetMapping()
+    public ApiResponse<MemberGetResponseDto> getMember(@CurrentMember Member member) {
+        MemberGetResponseDto response = memberService.getMember(member);
+        return ApiResponse.success(SuccessCode.READ_SUCCESS, response);
     }
 }
