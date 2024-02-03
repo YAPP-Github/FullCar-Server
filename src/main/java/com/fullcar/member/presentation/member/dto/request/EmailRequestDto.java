d34dfea0b8682d3d9dcb513d763ae9389aecfe6e
Seokyeong <purewater71@naver.com>
2024-02-04 04:19:26 +0900

    add: an example for swagger

--- src/main/java/com/fullcar/member/presentation/member/dto/request/EmailRequestDto.java
+++ src/main/java/com/fullcar/member/presentation/member/dto/request/EmailRequestDto.java
@@ -1,5 +1,6 @@
 package com.fullcar.member.presentation.member.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,5 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class EmailRequestDto {
+    @Schema(description = "회사 이메일", example = "whoareyou@yanolja.com")
     private String email;
 }
