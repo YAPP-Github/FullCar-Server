d21bc98b650bc7f27c7cc224c4c37fb81a0596ff
Young Hun Park <pyhpyh0670@gmail.com>
2024-02-05 00:35:18 +0900

    fix: Resolve conflict

--- src/main/java/com/fullcar/core/response/ErrorCode.java
+++ src/main/java/com/fullcar/core/response/ErrorCode.java
@@ -13,8 +13,6 @@ public enum ErrorCode {
 
     /* 400 BAD REQUEST */
     FAILED_TO_GENERATE_PUBLIC_KEY(BAD_REQUEST, "애플 공개키 생성 중 문제 발생"),
-    CANNOT_SEND_TO_OWN_CARPOOL(BAD_REQUEST, "자기자신의 카풀에는 신청할 수 없습니다."),
-    DUPLICATED_FORM(BAD_REQUEST, "이미 요청을 보낸 카풀입니다."),
 
     /* 401 UNAUTHORIZED */
     UNAUTHORIZED_KAKAO_TOKEN(UNAUTHORIZED, "유효하지 않은 카카오 토큰"),
